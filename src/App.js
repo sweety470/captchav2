6f1caa6bff37ef4675aab74dd0a58fda8d27fdfd
sweety470 <63448946+sweety470@users.noreply.github.com>
2021-08-24 19:57:27 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css';
+
+
+class App extends React.Component {
+  
+  render() {
+    
+    
+
+    return <h2>Recaptcha</h2>;
+    
+  }
+}
+
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,76 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ReCAPTCHA from "react-google-recaptcha";
+
+
+const TEST_SITE_KEY = "6LftxwwcAAAAAPIVnjFcZ_nnxRv7UpHevfAUq0s0";
+const DELAY = 3000;
+
+class App extends React.Component {
+  constructor(props, ...args) {
+    super(props, ...args);
+    this.state = {
+      callback: "not fired",
+      value: "[empty]",
+      load: false,
+      expired: "false"
+    };
+    this._reCaptchaRef = React.createRef();
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ load: true });
+    }, DELAY);
+    console.log("didMount - reCaptcha Ref-", this._reCaptchaRef);
+  }
+
+  handleChange = value => {
+    console.log("Captcha value:", value);
+    this.setState({ value });
+    // if value is null recaptcha expired
+    if (value === null) this.setState({ expired: "true" });
+  };
+
+  asyncScriptOnLoad = () => {
+    this.setState({ callback: "called!" });
+    console.log("scriptLoad - reCaptcha Ref-", this._reCaptchaRef);
+  };
+
+  render() {
+    const { value, callback, load, expired } = this.state || {};
+    return (
+      <div className="App">
+        <h1>
+          <a
+            href="https://github.com/dozoisch/react-google-recaptcha"
+            target="_blank"
+          >
+            
+          </a>
+          
+        </h1>
+        <h2>
+          NOTE: initial load delayed <em>{DELAY / 1000}sec</em> to demonstrate
+          callback
+        </h2>
+        <h3>Recaptcha loaded callback: {callback}</h3>
+        <h5>Recaptcha value: {value}</h5>
+        <h5>Expired: {expired}</h5>
+        {load && (
+          <ReCAPTCHA
+            style={{ display: "inline-block" }}
+            theme="dark"
+            ref={this._reCaptchaRef}
+            sitekey={TEST_SITE_KEY}
+            onChange={this.handleChange}
+            asyncScriptOnLoad={this.asyncScriptOnLoad}
+          />
+        )}
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
